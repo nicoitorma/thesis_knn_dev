153b5e4bce9fedc1ae1f55409f6836da341842b8
nicoitorma <nicoitorma38@gmail.com>
2023-08-29 20:20:05 +0800

    v0.1.0

--- lib/drawables/bg.dart
+++ lib/drawables/bg.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:th_knn/values/colors.dart';
 
 class BackgroundImage extends StatelessWidget {
   const BackgroundImage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                colors: [const Color.fromARGB(50, 58, 183, 241), darkBlue],
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft)));
+    // return Container(
+    //     decoration: BoxDecoration(
+    //         gradient: LinearGradient(
+    //             colors: [const Color.fromARGB(50, 58, 183, 241), darkBlue],
+    //             begin: Alignment.topRight,
+    //             end: Alignment.bottomLeft)));
 
-    // return Opacity(
-    //     opacity: 0.3,
-    //     child: Image.asset('assets/imgs/bg.png',
-    //         fit: BoxFit.cover,
-    //         height: MediaQuery.sizeOf(context).height,
-    //         width: MediaQuery.sizeOf(context).width));
+    return Opacity(
+        opacity: 0.3,
+        child: Image.asset('assets/imgs/bg.png',
+            fit: BoxFit.cover,
+            height: MediaQuery.sizeOf(context).height,
+            width: MediaQuery.sizeOf(context).width));
   }
 }
