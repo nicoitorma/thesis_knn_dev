0889aa062828901c8fc3925333baa4c99f5a0340
Nico <nicoitorma38@gmail.com>
2023-11-05 22:01:47 +0800

    Update rate_prediction.dart

--- lib/controllers/rate_prediction.dart
+++ lib/controllers/rate_prediction.dart
@@ -23,7 +23,7 @@ saveGradeToOnline(Rating predRating) async {
   try {
     /// This will add a new grades to the document [saved_grades]
     String docName =
-        '${predRating.program}_(${predRating.idNum})_(${predRating.rating} stars)_${predRating.expectedCareer}';
+        '${predRating.program}_(${predRating.idNum})_(${predRating.rating}stars)_${predRating.expectedCareer}';
     await _db.collection(labelCollection).doc(docName).set(dataMap);
   } catch (err) {
     debugPrint(err.toString());
